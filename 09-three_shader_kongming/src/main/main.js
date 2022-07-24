f10e74bb824f1f7ba98aa6fb78ebc34a8802f88e
Kusou1 <zhangshaohong1997@gmail.com>
2022-07-24 23:01:50 +0800

    shader 制作漫天飞舞孔明灯

---
+++ 09-three_shader_kongming/src/main/main.js
@@ -0,0 +1,141 @@
+import * as THREE from "three";
+
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import gsap from "gsap";
+import * as dat from "dat.gui";
+import vertexShader from "../shaders/flylight/vertex.glsl";
+import fragmentShader from "../shaders/flylight/fragment.glsl";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+// 目标：认识shader
+
+//创建gui对象
+const gui = new dat.GUI();
+
+// console.log(THREE);
+// 初始化场景
+const scene = new THREE.Scene();
+
+// 创建透视相机
+const camera = new THREE.PerspectiveCamera(
+  90,
+  window.innerHeight / window.innerHeight,
+  0.1,
+  1000
+);
+// 设置相机位置
+// object3d具有position，属性是1个3维的向量
+camera.position.set(0, 0, 2);
+// 更新摄像头
+camera.aspect = window.innerWidth / window.innerHeight;
+//   更新摄像机的投影矩阵
+camera.updateProjectionMatrix();
+scene.add(camera);
+
+// 加入辅助轴，帮助我们查看3维坐标轴
+// const axesHelper = new THREE.AxesHelper(5);
+// scene.add(axesHelper);
+
+// 加载纹理
+
+// 创建纹理加载器对象
+const rgbeLoader = new RGBELoader();
+rgbeLoader.loadAsync("./assets/2k.hdr").then((texture) => {
+  texture.mapping = THREE.EquirectangularReflectionMapping;
+  scene.background = texture;
+  scene.environment = texture;
+});
+
+// 创建着色器材质;
+const shaderMaterial = new THREE.ShaderMaterial({
+  vertexShader: vertexShader,
+  fragmentShader: fragmentShader,
+  uniforms: {},
+  side: THREE.DoubleSide,
+  //   transparent: true,
+});
+
+// 初始化渲染器
+const renderer = new THREE.WebGLRenderer({ alpha: true });
+// renderer.shadowMap.enabled = true;
+// renderer.shadowMap.type = THREE.BasicShadowMap;
+// renderer.shadowMap.type = THREE.VSMShadowMap;
+renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+// renderer.toneMapping = THREE.LinearToneMapping;
+// renderer.toneMapping = THREE.ReinhardToneMapping;
+// renderer.toneMapping = THREE.CineonToneMapping;
+renderer.toneMappingExposure = 0.2;
+
+const gltfLoader = new GLTFLoader();
+let LightBox = null;
+gltfLoader.load("./assets/model/flyLight.glb", (gltf) => {
+  console.log(gltf);
+
+  LightBox = gltf.scene.children[1];
+  LightBox.material = shaderMaterial;
+
+  for (let i = 0; i < 150; i++) {
+    let flyLight = gltf.scene.clone(true);
+    let x = (Math.random() - 0.5) * 300;
+    let z = (Math.random() - 0.5) * 300;
+    let y = Math.random() * 60 + 25;
+    flyLight.position.set(x, y, z);
+    gsap.to(flyLight.rotation, {
+      y: 2 * Math.PI,
+      duration: 10 + Math.random() * 30,
+      repeat: -1,
+    });
+    gsap.to(flyLight.position, {
+      x: "+=" + Math.random() * 5,
+      y: "+=" + Math.random() * 20,
+      yoyo: true,
+      duration: 5 + Math.random() * 10,
+      repeat: -1,
+    });
+    scene.add(flyLight);
+  }
+});
+
+// 设置渲染尺寸大小
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+// 监听屏幕大小改变的变化，设置渲染的尺寸
+window.addEventListener("resize", () => {
+  //   console.log("resize");
+  // 更新摄像头
+  camera.aspect = window.innerWidth / window.innerHeight;
+  //   更新摄像机的投影矩阵
+  camera.updateProjectionMatrix();
+
+  //   更新渲染器
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  //   设置渲染器的像素比例
+  renderer.setPixelRatio(window.devicePixelRatio);
+});
+
+// 将渲染器添加到body
+document.body.appendChild(renderer.domElement);
+
+// 初始化控制器
+const controls = new OrbitControls(camera, renderer.domElement);
+// 设置控制器阻尼
+controls.enableDamping = true;
+// 设置自动旋转
+controls.autoRotate = true;
+controls.autoRotateSpeed = 0.1;
+controls.maxPolarAngle = (Math.PI / 3) * 2;
+controls.minPolarAngle = (Math.PI / 3) * 2;
+
+const clock = new THREE.Clock();
+function animate(t) {
+  controls.update();
+  const elapsedTime = clock.getElapsedTime();
+
+  requestAnimationFrame(animate);
+  // 使用渲染器渲染相机看这个场景的内容渲染出来
+  renderer.render(scene, camera);
+}
+
+animate();
--- README.md
+++ README.md
@@ -9,6 +9,8 @@
 - [three.js_physics 应用物理引擎设置物体相互作用](./06-three_physics)
 - [three.js_shader Shader](./07-three_shader)
 - [three.js_shader_graph Shader glsl深入学习，编写各种类型的图案](./07-three_shader_graph)
+- [three.js_shader_kongming shader打造漫天飞舞孔明灯](./07-three_shader_kongming)
+- 
   
 ## WebGL是什么
 WebGL（Web 图形库）是一个 JavaScript API，可在任何兼容的 Web 浏览器中渲染高性能的交互式 3D 和 2D 图形，而无需使用插件。WebGL 通过引入一个与 OpenGL ES 2.0 非常一致的 API 来做到这一点，该 API 可以在 HTML5 <canvas>元素中使用。 这种一致性使 API 可以利用用户设备提供的硬件图形加速。
